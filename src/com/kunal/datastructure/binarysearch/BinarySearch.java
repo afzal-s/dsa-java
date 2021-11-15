7cb796320816dbbb2d2646f91307d7d12db2fb08
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-11-15 16:07:59 +0530

    DSA: Binary Search For Ascending And Descending Order.

--- src/com/kunal/datastructure/binarysearch/BinarySearch.java
+++ src/com/kunal/datastructure/binarysearch/BinarySearch.java
@@ -3,22 +3,27 @@ package com.kunal.datastructure.binarysearch;
 public class BinarySearch {
 
 	public static void main(String[] args) {
-		int[] arrayInSortedOrder = {-18, -12, -4, 0, 2, 5, 7, 8, 13, 15, 18, 22, 24}; 
-		int target = 15;
-		int answer = binarySearch(arrayInSortedOrder, target);
-		System.out.println(answer);
+		int[] arrayInSortedInAscendingOrder = {-18, -12, -4, 0, 2, 5, 7, 8, 13, 15, 18, 22, 24}; 
+		int targetOfAscendingOrder = 15;
+		int answerOfAscendingOrder = binarySearchForAscendingOrder(arrayInSortedInAscendingOrder, targetOfAscendingOrder);
+		System.out.println(answerOfAscendingOrder);
+		
+		int[] arrayInSortedInDescendingOrder = {24, 22, 18, 15, 13, 8, 7, 5, 2, 0, -4, -12, -18}; 
+		int targetOfDescendingOrder = 13;
+		int answerOfDescendingOrder = binarySearchForDescendingOrder(arrayInSortedInDescendingOrder, targetOfDescendingOrder);
+		System.out.println(answerOfDescendingOrder);
 	}
 
-	private static int binarySearch(int[] arrayInSortedOrder, int target) {
+	private static int binarySearchForAscendingOrder(int[] arrayInSortedInAscendingOrder, int target) {
 		int start = 0;
-		int end = arrayInSortedOrder.length - 1;
+		int end = arrayInSortedInAscendingOrder.length - 1;
 		
 		while (start <= end) {
 			int mid = start + (end - start) / 2;
 			
-			if (target < arrayInSortedOrder[mid]) {
+			if (target < arrayInSortedInAscendingOrder[mid]) {
 				end = mid - 1;
-			} else if (target > arrayInSortedOrder[mid]) {
+			} else if (target > arrayInSortedInAscendingOrder[mid]) {
 				start = mid + 1;
 			} else {
 				return mid;
@@ -28,4 +33,23 @@ public class BinarySearch {
 		return -1;
 	}
 
+	private static int binarySearchForDescendingOrder(int[] arrayInSortedInDescendingOrder,
+			int targetOfDescendingOrder) {
+		int start = 0;
+		int end = arrayInSortedInDescendingOrder.length - 1;
+		
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			
+			if (targetOfDescendingOrder > arrayInSortedInDescendingOrder[mid]) {
+				end = mid - 1;
+			} else if (targetOfDescendingOrder < arrayInSortedInDescendingOrder[mid]) {
+				start = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		
+		return -1;
+	}
 }
